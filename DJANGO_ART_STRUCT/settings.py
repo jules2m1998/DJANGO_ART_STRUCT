680ba171a42d37c9397bff21d2cd9c7395efdd66
jules2m1998 <mevaajules9@gmail.com>
2020-07-06 14:17:57 +0100

    Ajout de la configuration vuejs à django

--- DJANGO_ART_STRUCT/settings.py
+++ DJANGO_ART_STRUCT/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'webpack_loader'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "assets"),
+    os.path.join(BASE_DIR, "frontend/dist"),
+]
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'dist/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'frontend', 'webpack-stats.json')
+    }
+}
--- DJANGO_ART_STRUCT/urls.py
+++ DJANGO_ART_STRUCT/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from homepage.views import IndexTemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexTemplateView.as_view(), name="entry-point"),
 ]
